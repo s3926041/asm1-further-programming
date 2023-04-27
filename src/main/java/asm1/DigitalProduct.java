9f0fff7471859ca221b289e2dbf39a1a3117d7df
12er90 <hpioz123@gmail.com>
2023-04-27 20:25:04 +0700

    update

--- src/main/java/asm1/DigitalProduct.java
+++ src/main/java/asm1/DigitalProduct.java
@@ -2,8 +2,8 @@ package asm1;
 
 
 public class DigitalProduct extends Product {
-    public DigitalProduct(String name, String description, int quantity, double price) {
-        super(name, description, quantity, price, taxType, canBeGift);
+    public DigitalProduct(String name, String description, int quantity, double price,TaxType taxType, boolean canBeGift) {
+        super(name, description, quantity, price,taxType,canBeGift);
     }
 
     public String getType() {
